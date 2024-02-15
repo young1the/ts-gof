69225173a40bee3fc14e5837dc238de5dd227a00
young1the <youngthedev@gmail.com>
2024-02-15 13:17:36 +0900

    add docs

--- src/iterator/index.ts
+++ src/iterator/index.ts
@@ -1,3 +1,13 @@
+/*
+
+Iterator 패턴은 배열이나 Linked-List, Tree처럼 다른 데이터 구조를 갖는,
+Aggregator의 구성 항목을 표준화된 공통 API로 접근할 수 있습니다.
+
+데이터 구조가 다른 Aggregator일지라도 구성항목을 표준화 된 공통 API로 접근할 수 있음으로
+하나의 공통된 알고리즘으로 처리할 수 있습니다.
+
+*/
+
 import Aggregator from "./Aggregator";
 import ArrayIterator from "./ArrayIterator";
 import CustomArray from "./CustomArray";
--- src/strategy/index.ts
+++ src/strategy/index.ts
@@ -3,3 +3,13 @@ import LoopSumStrategy from "./LoopSumStrategy";
 import sumPrint from "./sumPrint";
 
 export {GaussSumStrategy, LoopSumStrategy, sumPrint}
+
+/*
+
+Iterator 패턴은 배열이나 Linked-List, Tree처럼 다른 데이터 구조를 갖는,
+Aggregator의 구성 항목을 표준화된 공통 API로 접근할 수 있습니다.
+
+데이터 구조가 다른 Aggregator일지라도 구성항목을 표준화 된 공통 API로 접근할 수 있음으로
+하나의 공통된 알고리즘으로 처리할 수 있습니다.
+
+*/
