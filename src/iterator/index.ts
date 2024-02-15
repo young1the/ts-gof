908ff0607e696a04b6cd04408bba7bb9b49559b6
young1the <youngthedev@gmail.com>
2024-02-15 10:25:38 +0900

    테스트 환경 구성 + jest.config에서 preset을 활성화 해야함

---
+++ src/iterator/index.ts
@@ -0,0 +1,7 @@
+import Aggregator from "./Aggregator";
+import ArrayIterator from "./ArrayIterator";
+import CustomArray from "./CustomArray";
+import CustomIterator from "./CustomIterator";
+import Item from "./Item";
+
+export { Aggregator, ArrayIterator, CustomIterator, CustomArray, Item }
